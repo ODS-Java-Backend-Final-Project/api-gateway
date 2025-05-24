bcd30673ac693e1c78ae3bfa8d870b14c73c3227
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-24 19:22:30 +0200

    inittial commit

---
+++ src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.example.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+//                .route("board-game_service", r -> r.path("/board-games/**")
+//                        .uri("lb://board-game-service"))
+//                .route("event_service", r -> r.path("/events/**")
+//                        .uri("lb://event-service"))
+
+                        .build();
+    }
+}
